9200d333540bf08d829237e0a1443388d4031274
vagrant <vagrant@vagrant.vm>
2018-09-27 21:25:05 +0000

    Add basic templates. Create and show categories

--- web.py
+++ web.py
@@ -1,56 +1,75 @@
 #!/usr/bin/python3
-from flask import Flask
+
+#Imports
+
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from model import Base, User, Game, GameCategory
 
 app = Flask(__name__)
 
 # Web application - Games catalog
 
+engine = create_engine('sqlite:///gamescatalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 # Read endpoints
 
 @app.route("/")
 def showMainPage():
+    categories = session.query(GameCategory).all()
+    return render_template("category.html", categories = categories)
 
-    return ""
-
-@app.route("/catalog/<string:category>/items")
+@app.route("/category/<string:category>/items")
 def showSelectedItems():
-
+    
     return ""
 
-@app.route("/catalog/<string:category>/<string:item>")
+@app.route("/category/<string:category>/<string:item>")
 def showSelectedItem():
 
     return ""
 
 # Create endpoints
 
-@app.route("/catalog/new")
+@app.route("/category/new", methods=['GET', 'POST'])
 def createNewGameCategory():
+    if request.method == 'POST':
+        gameGategory = GameCategory(name=request.form["categoryname"])
+        session.add(gameGategory)
+        session.commit()
+        return render_template("index.html")
+    else:
+        return render_template('newcategory.html')
 
-    return ""
-
-@app.route("/catalog/<string:item>/new")
+@app.route("/category/<string:item>/new", methods=['GET', 'POST'])
 def createNewItem():
-
-    return ""
+    if request.method == 'POST':
+        return render_template('index.html')
+    else:
+        return render_template('newgame.html')
 
 # Delete endpoints
 
-@app.route("/catalog/<string:item>/delete")
+@app.route("/category/<string:item>/delete")
 def deleteSelectedItem():
 
     return ""
 
 # Update endpoints
 
-@app.route("/catalog/<string:item>/edit")
+@app.route("/category/<string:item>/edit")
 def editSelectedItem():
 
     return ""
 
 # API - Json
 
-@app.route("/catalog.json")
+@app.route("/category.json")
 def showContentInJson():
 
     return ""

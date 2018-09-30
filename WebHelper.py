c565ec6d784076d564091af32753f9c401c8f05c
Artur <helshtzn@gmail.com>
2018-09-30 20:28:54 +0200

    Add Google OAuth2.

---
+++ WebHelper.py
@@ -0,0 +1,13 @@
+import random, string, json
+from flask import make_response
+
+class WebHelper:
+    def generateRandomClientId(self, rndNumb):
+         # Generate random chain of strings and digits with length of 64 signs which will be our antiforgery state
+        state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(rndNumb))
+        return state
+
+    def generateJsonDump(self, description, error_code):
+        response = make_response(json.dumps(description), error_code)
+        response.headers['Content-Type'] = 'application/json'
+        return response
--- web.py
+++ web.py
@@ -3,10 +3,21 @@
 #Imports
 
 from flask import Flask, render_template, request, redirect, url_for
+from flask import session as login_session
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from model import Base, User, Game, GameCategory
 from DbOperations import DbOperations
+import httplib2
+import json
+from flask import make_response
+import requests
+from WebHelper import WebHelper
+
+# Import OAuth2 for Google
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
 
 app = Flask(__name__)
 
@@ -21,6 +32,119 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 dbOperations = DbOperations()
+webHelper = WebHelper()
+
+# Define Client ID
+
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+
+# Login / Logout endpoints (Google Accounts)
+
+@app.route("/login")
+def logIn():
+    login_session['state'] = webHelper.generateRandomClientId(64)
+    return render_template('login.html', STATE=login_session['state'])
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        return webHelper.generateJsonDump('Invalid state parameter.', 401)
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        return webHelper.generateJsonDump('Failed to upgrade the authorization code.', 401)
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={}').format(access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1].decode('utf-8'))
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        return webHelper.generateJsonDump("Token's user ID doesn't match given user ID.", 401)
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        return webHelper.generateJsonDump("Token's client ID does not match app's.", 401)
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        return webHelper.generateJsonDump('Current user is already connected.', 200)
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    print("done!")
+    return output
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # revoke current token
+    # access_token = credentials.access_token
+    print("{}{}{}".format("start:", access_token, ":end"))
+    url = 'https://accounts.google.com/o/oauth2/revoke?token={}'.format(access_token)
+    # print(url)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print('result is {}'.format(result))
+    print(result['status'])
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+@app.route("/logout")
+def logOut():
+
+    return ""
 
 # Read endpoints
 
@@ -94,5 +218,6 @@ def showContentInJson():
     return ""
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 8000)

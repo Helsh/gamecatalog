ea31cd83bad1fbda8fcd1542f67a01cca15bb0f0
Artur <helshtzn@gmail.com>
2018-10-01 20:54:04 +0200

    Add comments to operations on database

--- DbOperations.py
+++ DbOperations.py
@@ -10,15 +10,18 @@ session = DBSession()
 
 class DbOperations:
     def addRecord(self, item):
+        # Add any record
         session.add(item)
         session.commit()
 
     def removeRecordById(self, game_id):
+        # Find game by id and remove
         game = session.query(Game).filter_by(id = game_id).first()
         session.delete(game)
         session.commit()
 
     def findUserByEmail(self, user_email):
+        # Find and return user or None
         user = session.query(User).filter_by(email = user_email).first()
         return user
 
